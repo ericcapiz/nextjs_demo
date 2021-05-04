9e2500f9ff21d13462245208492963886604f3e5
ericcapiz <ericcapiz@gmail.com>
2021-05-04 15:36:04 -0500

    nav added, mobile drawer started

--- src/components/header/mobile-drawer.js
+++ src/components/header/mobile-drawer.js
@@ -27,8 +27,23 @@ const social = [
 ];
 
 export default function MobileDrawer() {
+  const [isDrawerOpen, setIsDrawerOpen] = useState(false);
   return (
-   <h1>Mobile Drawer</h1>
+   <Drawer 
+   width="320px" 
+   drawerHandler={
+     <Box sx={styles.handler}>
+       <IoMdMenu size="26px"/>
+     </Box>
+   }
+   open={isDrawerOpen}
+   toggleHandler={() => setIsDrawerOpen((prevState) => !prevState)}
+   closeButton={<IoMdClose size="24px" color="#000000" />}
+   drawerStyle={styles.drawer}
+   closeBtnStyle={styles.close}
+   >
+
+   </Drawer>
   );
 };
 
