fc0a0d9d5b609f334f78db518cec73b8b705ca2d
ericcapiz <ericcapiz@gmail.com>
2021-05-05 19:32:56 -0500

    pricing section started

--- src/components/price-card.js
+++ src/components/price-card.js
@@ -14,7 +14,28 @@ export default function PriceCard({
   },
 }) {
   return (
-    <h1>PriceCard</h1>
+    <Card className={header ? 'package__card active' : 'package__card'} sx={styles.pricingBox}>
+      {header && (<Text sx={styles.header}>{header}</Text>)}
+      <Box>
+        <Box className="package__header" sx={styles.pricingHeader}>
+          <Heading className="package__name" variant="title">
+            {name}
+          </Heading>
+          <Text as="p">{description}</Text>
+        </Box>
+        <List items={points} childStyle={styles.listItem} />
+        <Text className="package__price" sx={styles.price}>
+          {priceWithUnit}
+          <span>/Monthly</span>
+        </Text>
+        <Box sx={styles.buttonGroup}>
+          <Button variant="primary" aria-label={buttonText}>{buttonText}</Button>
+          {anotherOption && (
+            <Button sx={{color: 'black'}} variant="textButton" className="free__trial" aria-label={anotherOption}>{anotherOption}</Button>
+          )}
+        </Box>
+      </Box>
+    </Card>
   );
 }
 
